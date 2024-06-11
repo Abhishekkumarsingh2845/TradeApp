ed0728a4083744edc18149deda2bc0430cb60611
Abhishekkumarsingh2845 <109286249+Abhishekkumarsingh2845@users.noreply.github.com>
2024-06-11 17:14:56 +0530

    completing the Home screen

--- Src/Screens/Home.js
+++ Src/Screens/Home.js
@@ -4,6 +4,7 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from 'react-native';
 import React from 'react';
@@ -70,17 +71,203 @@ const Home = () => {
           </View>
         </ScrollView>
       </View>
-      <View style={{backgroundColor:'green'}}>
-      <View style={{flexDirection: 'row'}}>
-        <View>
-          <Image source={require('./../Assets/Img/goto.png')} style={{width:35,height:30,resizeMode:'contain'}} />
+      <ScrollView>
+        <View style={styles.cardContainer}>
+          <View style={styles.card}>
+            <View style={styles.cardHeader}>
+              <Image
+                source={require('./../Assets/Img/goto.png')}
+                style={styles.cardImage}
+              />
+              <View style={styles.textRow}>
+                <View style={{flexDirection: 'row'}}>
+                  <Text style={styles.baseText}>VEECS</Text>
+                  <Text style={styles.superscript}>(56 AUD/Unit)</Text>
+                </View>
+                <View style={{flexDirection: 'row'}}>
+                  <Text style={{fontSize: hp(2), color: 'grey'}}>
+                    $200/100U US $200.00
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.cardRightText}>
+                <Text style={{fontSize: hp(2.5), color: 'black'}}>
+                  $2,509.75
+                </Text>
+                <Text
+                  style={{fontSize: hp(2), color: 'black', color: '#21BF73'}}>
+                  +9.77%
+                </Text>
+              </View>
+            </View>
+            <View style={styles.cardActions}>
+              <TouchableOpacity
+                style={[styles.actionButton, styles.sellButton]}>
+                <Text style={styles.actionButtonText}>SELL</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={[styles.actionButton, styles.buyButton]}>
+                <Text style={styles.actionButtonText}>BUY</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
-        <View style={styles.textRow}>
-          <Text style={styles.baseText}>VEECS</Text>
-          <Text style={styles.superscript}>(56 AUD/Unit)</Text>
+        <View style={styles.cardContainer}>
+          <View style={styles.card}>
+            <View style={styles.cardHeader}>
+              <Image
+                source={require('./../Assets/Img/goto.png')}
+                style={styles.cardImage}
+              />
+              <View style={styles.textRow}>
+                <View style={{flexDirection: 'row'}}>
+                  <Text style={styles.baseText}>VEECS</Text>
+                  <Text style={styles.superscript}>(56 AUD/Unit)</Text>
+                </View>
+                <View style={{flexDirection: 'row'}}>
+                  <Text style={{fontSize: hp(2), color: 'grey'}}>
+                    $200/100U US $200.00
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.cardRightText}>
+                <Text style={{fontSize: hp(2.5), color: 'black'}}>
+                  $2,509.75
+                </Text>
+                <Text
+                  style={{fontSize: hp(2), color: 'black', color: '#21BF73'}}>
+                  +9.77%
+                </Text>
+              </View>
+            </View>
+            <View style={styles.cardActions}>
+              <TouchableOpacity
+                style={[styles.actionButton, styles.sellButton]}>
+                <Text style={styles.actionButtonText}>SELL</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={[styles.actionButton, styles.buyButton]}>
+                <Text style={styles.actionButtonText}>BUY</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
-      </View>
-      </View>
+        <View style={styles.cardContainer}>
+          <View style={styles.card}>
+            <View style={styles.cardHeader}>
+              <Image
+                source={require('./../Assets/Img/goto.png')}
+                style={styles.cardImage}
+              />
+              <View style={styles.textRow}>
+                <View style={{flexDirection: 'row'}}>
+                  <Text style={styles.baseText}>VEECS</Text>
+                  <Text style={styles.superscript}>(56 AUD/Unit)</Text>
+                </View>
+                <View style={{flexDirection: 'row'}}>
+                  <Text style={{fontSize: hp(2), color: 'grey'}}>
+                    $200/100U US $200.00
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.cardRightText}>
+                <Text style={{fontSize: hp(2.5), color: 'black'}}>
+                  $2,509.75
+                </Text>
+                <Text
+                  style={{fontSize: hp(2), color: 'black', color: '#21BF73'}}>
+                  +9.77%
+                </Text>
+              </View>
+            </View>
+            <View style={styles.cardActions}>
+              <TouchableOpacity
+                style={[styles.actionButton, styles.sellButton]}>
+                <Text style={styles.actionButtonText}>SELL</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={[styles.actionButton, styles.buyButton]}>
+                <Text style={styles.actionButtonText}>BUY</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+        <View style={styles.cardContainer}>
+          <View style={styles.card}>
+            <View style={styles.cardHeader}>
+              <Image
+                source={require('./../Assets/Img/goto.png')}
+                style={styles.cardImage}
+              />
+              <View style={styles.textRow}>
+                <View style={{flexDirection: 'row'}}>
+                  <Text style={styles.baseText}>VEECS</Text>
+                  <Text style={styles.superscript}>(56 AUD/Unit)</Text>
+                </View>
+                <View style={{flexDirection: 'row'}}>
+                  <Text style={{fontSize: hp(2), color: 'grey'}}>
+                    $200/100U US $200.00
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.cardRightText}>
+                <Text style={{fontSize: hp(2.5), color: 'black'}}>
+                  $2,509.75
+                </Text>
+                <Text
+                  style={{fontSize: hp(2), color: 'black', color: '#21BF73'}}>
+                  +9.77%
+                </Text>
+              </View>
+            </View>
+            <View style={styles.cardActions}>
+              <TouchableOpacity
+                style={[styles.actionButton, styles.sellButton]}>
+                <Text style={styles.actionButtonText}>SELL</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={[styles.actionButton, styles.buyButton]}>
+                <Text style={styles.actionButtonText}>BUY</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+        <View style={styles.cardContainer}>
+          <View style={styles.card}>
+            <View style={styles.cardHeader}>
+              <Image
+                source={require('./../Assets/Img/goto.png')}
+                style={styles.cardImage}
+              />
+              <View style={styles.textRow}>
+                <View style={{flexDirection: 'row'}}>
+                  <Text style={styles.baseText}>VEECS</Text>
+                  <Text style={styles.superscript}>(56 AUD/Unit)</Text>
+                </View>
+                <View style={{flexDirection: 'row'}}>
+                  <Text style={{fontSize: hp(2), color: 'grey'}}>
+                    $200/100U US $200.00
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.cardRightText}>
+                <Text style={{fontSize: hp(2.5), color: 'black'}}>
+                  $2,509.75
+                </Text>
+                <Text
+                  style={{fontSize: hp(2), color: 'black', color: '#21BF73'}}>
+                  +9.77%
+                </Text>
+              </View>
+            </View>
+            <View style={styles.cardActions}>
+              <TouchableOpacity
+                style={[styles.actionButton, styles.sellButton]}>
+                <Text style={styles.actionButtonText}>SELL</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={[styles.actionButton, styles.buyButton]}>
+                <Text style={styles.actionButtonText}>BUY</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -90,7 +277,7 @@ export default Home;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 0, // No horizontal padding
+    paddingHorizontal: 0,
     backgroundColor: 'white',
   },
   header: {
@@ -122,15 +309,13 @@ const styles = StyleSheet.create({
   secondContainer: {
     borderWidth: 0.4,
     borderColor: '#8A8181',
-    width: '100%', // Ensure it spans the full width
-
+    width: '100%',
     marginVertical: hp(1.5),
   },
   trendingText: {
     color: 'black',
     paddingHorizontal: wp(3.5),
     marginVertical: hp(1),
-
     fontWeight: '400',
   },
   scrollView: {
@@ -142,8 +327,30 @@ const styles = StyleSheet.create({
     backgroundColor: 'whitesmoke',
     borderRadius: 7,
     elevation: 4,
-
-    marginRight: wp(2), // Add some space between the children
+    marginRight: wp(2),
+  },
+  cardContainer: {
+    marginHorizontal: wp(1),
+    marginVertical: hp(1),
+  },
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 12,
+    elevation: 4,
+    padding: hp(1),
+  },
+  cardHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  cardImage: {
+    width: 45,
+    height: 45,
+    resizeMode: 'contain',
+  },
+  textRow: {
+    alignItems: 'baseline',
+    marginLeft: wp(2),
   },
   baseText: {
     fontSize: hp(2.5),
@@ -153,7 +360,33 @@ const styles = StyleSheet.create({
   superscript: {
     fontSize: hp(1.8),
     lineHeight: 24,
-    textAlignVertical: 'top',
     color: 'grey',
+  },
+  cardRightText: {
+    position: 'absolute',
+    right: 0,
+    alignItems: 'flex-end',
+  },
+  cardActions: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: wp(3),
+
+    marginTop: hp(2),
+  },
+  actionButton: {
+    paddingHorizontal: wp(16),
+    paddingVertical: hp(1.5),
+    borderRadius: 10,
+  },
+  sellButton: {
+    backgroundColor: Colours.primaryred,
+  },
+  buyButton: {
+    backgroundColor: Colours.primary,
+  },
+  actionButtonText: {
+    fontSize: 19,
+    color:Colours.white,
   },
 });
